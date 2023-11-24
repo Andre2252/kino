cbf39c67f68e9e883fc446a7f351dd9f3804228b
Andre2252 <andrkostyuchenko@mail.ru>
2023-11-24 21:44:23 +0300

    подключил файлы страниц сайта

--- config/routes.php
+++ config/routes.php
@@ -2,12 +2,18 @@
 
 //use App\Router\Route;
 
-//return [
+return [
+   '/home' => function() {
+      include_once __DIR__ . '/../views/pages/home.php';
+   },
+   '/movies' => function() {
+      include_once __DIR__ . '/../views/pages/movies.php';
+   },
    //Route::get('/home', function () {
       //include_once APP_PATH.'/views/pages/home.php';
    //}),
    //Route::get('/movies', function () {
       //include_once APP_PATH.'/views/pages/movies.php';
    //}),
-//];
+];
 ?>
