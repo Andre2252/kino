084dfd39ac7c3bdc11a9f22ed35082243530cf55
Andre2252 <andrkostyuchenko@mail.ru>
2023-11-28 01:53:36 +0300

    подключил файл style.css

--- views/pages/home.php
+++ views/pages/home.php
@@ -1,1 +1,12 @@
-<h1>Home page</h1>
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+   <meta charset="UTF-8">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>Document</title>
+   <link rel="stylesheet" href="/public/assets/css/app.css">
+</head>
+<body>
+   <h1>Home page</h1>
+</body>
+</html>
