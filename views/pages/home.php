c6b4b028f0f35ba1cdea96a8b06b9c8c444eaddb
Andre2252 <andrkostyuchenko@mail.ru>
2023-11-28 02:15:24 +0300

    подключил файл start.php, end.php

--- views/pages/home.php
+++ views/pages/home.php
@@ -1,12 +1,3 @@
-<!DOCTYPE html>
-<html lang="ru">
-<head>
-   <meta charset="UTF-8">
-   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-   <title>Document</title>
-   <link rel="stylesheet" href="/public/assets/css/app.css">
-</head>
-<body>
+<?php include __DIR__.'/../components/start.php' ?>
    <h1>Home page</h1>
-</body>
-</html>
+<?php include __DIR__.'/../components/end.php' ?>
