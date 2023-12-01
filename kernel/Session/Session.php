6fa31d6428b609c4f00f6f4dd4d2c0de188382f9
Andre2252 <andrkostyuchenko@mail.ru>
2023-12-01 18:14:18 +0300

    добавил несколько функций

--- kernel/Session/Session.php
+++ kernel/Session/Session.php
@@ -1,3 +1,37 @@
 <?php
-   
+
+   namespace App\Kernel\Session;
+
+   class Session
+   {
+      public function __construct()
+      {
+         session_start();
+      }
+
+      public function set(string $key, $value): void
+      {
+         $_SESSION[$key] = $value;
+      }
+
+      public function get(string $key, $default = null)
+      {
+         return $_SESSION[$key] ?? $default;
+      }
+
+      public function has(string $key): bool
+      {
+         return isset($_SESSION[$key]);
+      }
+
+      public function remove(string $key): void
+      {
+         unset($_SESSION[$key]);
+      }
+
+      public function destroy(): void
+      {
+         session_destroy();
+      }
+   }
 ?>
