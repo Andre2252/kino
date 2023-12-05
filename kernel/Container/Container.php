bec0e34d3cb4b57084eb09384ac768bf7b2fd8eb
Andre2252 <andrkostyuchenko@mail.ru>
2023-12-06 01:08:15 +0300

    добавил дополнения

--- kernel/Container/Container.php
+++ kernel/Container/Container.php
@@ -2,17 +2,21 @@
 
 namespace App\Kernel\Container;
 
+use App\Kernel\Config\Config;
+use App\Kernel\Config\ConfigInterface;
+use App\Kernel\Database\Database;
+use App\Kernel\Database\DatabaseInterface;
 use App\Kernel\Http\Redirect;
 use App\Kernel\Http\RedirectInterface;
 use App\Kernel\Http\Request;
 use App\Kernel\Http\RequestInterface;
 use App\Kernel\Router\Router;
 use App\Kernel\Router\RouterInterface;
-use App\Kernel\Validator\Validator;
-use App\Kernel\View\View;
 use App\Kernel\Session\Session;
 use App\Kernel\Session\SessionInterface;
+use App\Kernel\Validator\Validator;
 use App\Kernel\Validator\ValidatorInterface;
+use App\Kernel\View\View;
 use App\Kernel\View\ViewInterface;
 
 class Container
@@ -29,6 +33,10 @@ class Container
 
    public readonly SessionInterface $session;
 
+   public readonly ConfigInterface $config;
+
+   public readonly DatabaseInterface $database;
+
    public function __construct()
    {
       $this->registerServices();
@@ -43,6 +51,8 @@ class Container
       $this->session = new Session();
       $this->view = new View($this->session);
       $this->router = new Router($this->view, $this->request, $this->redirect, $this->session);
+      $this->config = new Config();
+      $this->database = new Database($this->config);
    }
 }
 
--- kernel/Database/Database.php
+++ kernel/Database/Database.php
@@ -2,23 +2,42 @@
    
    namespace App\Kernel\Database;
 
+   use App\Kernel\Config\ConfigInterface;
+
    class Database implements DatabaseInterface
    {
       private \PDO $pdo;
 
-      public function __construct()
-      {
+      public function __construct(
+         private ConfigInterface $config
+      ) {
          $this->connect();
       }
 
       public function insert(string $table, array $data): int|false
       {
-         // TODO: Implement insert() method.
+        // TODO: Implement insert() method.
       }
 
       private function connect(): void
       {
-         $this->pdo = new \PDO('mysql:host=localhost;port=3306;dbname=movie_db;charset=utf8', 'root', 'root');
+         $driver = $this->config->get('database.driver');
+         $host = $this->config->get('database.host');
+         $port = $this->config->get('database.port');
+         $database = $this->config->get('database.database');
+         $username = $this->config->get('database.username');
+         $password = $this->config->get('database.password');
+         $charset = $this->config->get('database.charset');
+
+         try {
+            $this->pdo = new \PDO(
+               "$driver:host=$host;port=$port;dbname=$database;charset=$charset", 
+               $username, 
+               $password
+            );
+         } catch (\PDOException $exception) {
+            exit("Database connection failed (Не удалось подключиться к базе данных): {$exception->getMessage()}");
+         }
       }
    }
 ?>
