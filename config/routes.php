321d2f7e2fc789920cc984b90550108bdd1398e8
Andre2252 <andrkostyuchenko@mail.ru>
2023-12-06 19:32:09 +0300

    добавил страницу login

--- config/routes.php
+++ config/routes.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Controllers\HomeController;
+use App\Controllers\LoginController;
 use App\Controllers\MovieController;
 use App\Controllers\RegisterController;
 use App\Kernel\Router\Route;
@@ -11,5 +12,8 @@ return [
    Route::get('/admin/movies/add', [MovieController::class, 'add']),
    Route::post('/admin/movies/add', [MovieController::class, 'store']),
    Route::get('/register', [RegisterController::class, 'index']),
+   Route::post('/register', [RegisterController::class, 'register']),
+   Route::get('/login', [LoginController::class, 'index']),
+   Route::post('/login', [LoginController::class, 'login']),
 ];
 ?>
