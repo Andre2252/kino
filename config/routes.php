cbef55b9d0b058bd6e7d3973105e9e5eb8345ed6
Andre2252 <andrkostyuchenko@mail.ru>
2023-12-06 16:17:42 +0300

    добавил метод index

--- config/routes.php
+++ config/routes.php
@@ -10,6 +10,6 @@ return [
    Route::get('/movies', [MovieController::class, 'index']),
    Route::get('/admin/movies/add', [MovieController::class, 'add']),
    Route::post('/admin/movies/add', [MovieController::class, 'store']),
-   Route::get('/register', [RegisterController::class, 'store']),
+   Route::get('/register', [RegisterController::class, 'index']),
 ];
 ?>
