81ce02e262243b72ffb6f72e00e86f2d676d0cf0
Andre2252 <andrkostyuchenko@mail.ru>
2023-12-06 19:33:12 +0300

    внесена корректировка

--- kernel/View/View.php
+++ kernel/View/View.php
@@ -3,8 +3,8 @@
    namespace App\Kernel\View;
 
    use App\Kernel\Exceptions\ViewNotFoundException;
-   use App\Kernel\Session\Session;
-use App\Kernel\Session\SessionInterface;
+   //use App\Kernel\Session\Session;
+   use App\Kernel\Session\SessionInterface;
 
    class View implements ViewInterface
    {
