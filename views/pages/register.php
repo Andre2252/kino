fbcd145bb1d3cc2d789626872158d286b3d1ee33
Andre2252 <andrkostyuchenko@mail.ru>
2023-12-06 19:54:51 +0300

    внесена корректировка

--- views/pages/register.php
+++ views/pages/register.php
@@ -7,25 +7,27 @@
 <?php $view->component(name:'start') ?>
    <h1>Register</h1>
 
-<form action="/register" method="POST">
-   <p>Email-адрес</p>
-   <input type="text" name="email">
-   <?php if($session->has('email')) { ?>
-         <ul>
-            <?php foreach ($session->getFlash(key: 'email') as $error) { ?>
-               <li style="color: red;"><?php echo $error ?></li>
-            <?php } ?>
-         </ul>
-      <?php } ?>
-   <p>Password</p>
-   <input type="text" name="password">
-   <?php if($session->has('password')) { ?>
+   <form action="/register" method="POST">
+      <p>Email-адрес</p>
+      <input type="text" name="email">
+      <?php if($session->has('email')) { ?>
+            <ul>
+               <?php foreach ($session->getFlash(key: 'email') as $error) { ?>
+                  <li style="color: red;"><?php echo $error ?></li>
+               <?php } ?>
+            </ul>
+         <?php } ?>
+      <p>Password</p>
+      <input type="text" name="password">
+      <?php if($session->has('password')) { ?>
          <ul>
             <?php foreach ($session->getFlash(key: 'password') as $error) { ?>
                <li style="color: red;"><?php echo $error ?></li>
             <?php } ?>
          </ul>
       <?php } ?>
-   <button>Register</button>
-</form>
+      <div>
+         <button>Register</button>
+      </div>
+   </form>
 <?php $view->component(name:'end') ?>
