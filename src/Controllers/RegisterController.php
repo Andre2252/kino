a112622172da1a60d576c10b82917ba6069bce08
Andre2252 <andrkostyuchenko@mail.ru>
2023-12-06 19:34:43 +0300

    Дописал :void

--- src/Controllers/RegisterController.php
+++ src/Controllers/RegisterController.php
@@ -6,9 +6,31 @@ use App\Kernel\Controller\Controller;
 
 class RegisterController extends Controller
 {
-   public function index()
+   public function index(): void
    {
-      $this->view('register');
+      $this->view(name: 'register');
+   }
+
+   public function register() //отвечает за добавление пользователя в базу
+   {
+      $validation = $this->request()->validate([
+         'email' => ['required', 'email'],
+         'password' => ['required', 'min:8']
+      ]);
+
+      if(! $validation) {
+         foreach ($this->request()->errors() as $field => $errors) {
+            $this->session()->set($field, $errors);
+         }
+         $this->redirect('/register');
+      }
+
+      $userId = $this->db()->insert('users', [
+         'email' => $this->request()->input('email'),
+         'password' => password_hash($this->request()->input(key: 'password'), algo: PASSWORD_DEFAULT),
+      ]);
+
+      dd('User created with id: ' . $userId);
    }
 }
 ?>
